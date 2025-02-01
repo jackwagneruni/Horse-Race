01d8e45fbd7fa62739abd962d1112939559b1532
jackwagneruni <jackwagneruni@gmail.com>
2025-02-01 00:38:53 +0000

    final draft push from codespace

--- horse_race.cpp
+++ horse_race.cpp
@@ -1,18 +1,49 @@
-#include <random>
-std::random_device rd;
-std::uniform_int_distribution<int> dist(0, 1);
+#include <iostream>
+#include <vector>
+#include <cstdlib>
 
+using namespace std;
 
- 
- 
- void advance(int horseNum, int* horses);
- void printLane(int horseNum, int* horses);
- bool isWinner(int horseNum, int* horses);'
- 
- // coin = dist(rd);
+const int number_of_horses = 5;
+const int length_of_track = 15;
 
+void printTrack(const vector<int>& positions) {
+    system("clear"); // Clears the console screen
+    for (int outer_loop = 0; outer_loop < number_of_horses; outer_loop++) {
+        for (int inner_loop = 0; inner_loop < length_of_track; inner_loop++) {
+            if (positions[outer_loop] == inner_loop)
+                cout << outer_loop;
+            else
+                cout << ".";
+        }
+        cout << endl;
+    }
+}
 
- int main(){
- 
- 
- }
+int main() {
+    srand(time(0));
+    vector<int> positions(number_of_horses, 0);
+    bool repeat = false;
+    int winner = -1;
+
+    while (!repeat) {
+        printTrack(positions);
+        cout << "Press enter for another turn";
+        cin.get();
+
+        for (int i = 0; i < number_of_horses; i++){
+            if (rand() % 2 == 1) { // Random movement
+                positions[i]++;
+            } // end of if statement
+            if (positions[i] >= length_of_track - 1) {
+                repeat = true;
+                winner = i;
+            } // end of if statement
+        } // end of for loop
+    } // end of the while repeat loop
+
+    // show the winner
+    printTrack(positions);
+    cout << "Horse " << winner << " is the winner." << endl;
+    return 0;
+}
