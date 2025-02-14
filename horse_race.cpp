08f8dd7b54334c16c4988e9764da002ccdea2e35
jackwagneruni <jackwagneruni@gmail.com>
2025-02-14 21:46:01 +0000

    Removed vectors

--- horse_race.cpp
+++ horse_race.cpp
@@ -1,49 +1,47 @@
 #include <iostream>
-#include <vector>
 #include <cstdlib>
-
-using namespace std;
+#include <ctime>
 
 const int number_of_horses = 5;
 const int length_of_track = 15;
 
-void printTrack(const vector<int>& positions) {
-    system("clear");
+void printTrack(const int positions[]) {
+    std::system("clear"); // Clears the console
     for (int outer_loop = 0; outer_loop < number_of_horses; outer_loop++) {
         for (int inner_loop = 0; inner_loop < length_of_track; inner_loop++) {
             if (positions[outer_loop] == inner_loop)
-                cout << outer_loop;
+                std::cout << outer_loop;
             else
-                cout << ".";
-        } // end of inner for loop
-        cout << endl;
-    } // end of outer for loop
-} // end of printTrack function
+                std::cout << ".";
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main() {
-    srand(time(0));
-    vector<int> positions(number_of_horses, 0);
+    std::srand(static_cast<unsigned>(std::time(0)));
+    int positions[number_of_horses] = {0};
     bool repeat = false;
     int winner = -1;
 
     while (!repeat) {
         printTrack(positions);
-        cout << "Press enter for another turn";
-        cin.get();
+        std::cout << "Press enter for another turn";
+        std::cin.get();
 
-        for (int i = 0; i < number_of_horses; i++){
-            if (rand() % 2 == 1) { // Random movement
+        for (int i = 0; i < number_of_horses; i++) {
+            if (std::rand() % 2 == 1) { // Random do or do not move forward
                 positions[i]++;
-            } // end of if statement
+            }
             if (positions[i] >= length_of_track - 1) {
                 repeat = true;
                 winner = i;
-            } // end of if statement
-        } // end of for loop
-    } // end of the while repeat loop
+            }
+        }
+    }
 
-    // show the winner
+    // Show the winner
     printTrack(positions);
-    cout << "Horse " << winner << " is the winner." << endl;
+    std::cout << "Horse " << winner << " is the winner." << std::endl;
     return 0;
 }
